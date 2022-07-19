22f2d17499751f27aacacc713184896a70e1b744
Diego <diego@studiohakuna.com>
2022-07-19 15:24:12 -0300

    style: footer mobile layout

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
   <div class="is-flex is-flex-direction-column content has-text-centered is-justify-content-center is-align-items-center is-align-self-center">
-    <div class="is-flex is-align-items-center mb-4">
+    <div class="footer-title is-flex is-align-items-center mb-4">
       <h3 class="title mr-2 my-0" type="is-primary">plántula</h3>
       <p>
         es el Proyecto Senior de Diego Salischiker en la Lic. en Tecnología Multimedial.
       </p>
     </div>
-    <p class="is-flex is-align-items-center">
+    <div class="container is-flex is-align-items-center">
       <a href="https://github.com/DSalischiker/plantula-backend" target="_blank" class="is-flex is-align-items-center">
         <svg width="36" height="36" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" enable-background="new 0 0 50 50"><path fill-rule="evenodd" clip-rule="evenodd" fill="#FAF9F1" d="M25 10c-8.3 0-15 6.7-15 15 0 6.6 4.3 12.2 10.3 14.2.8.1 1-.3 1-.7v-2.6c-4.2.9-5.1-2-5.1-2-.7-1.7-1.7-2.2-1.7-2.2-1.4-.9.1-.9.1-.9 1.5.1 2.3 1.5 2.3 1.5 1.3 2.3 3.5 1.6 4.4 1.2.1-1 .5-1.6 1-2-3.3-.4-6.8-1.7-6.8-7.4 0-1.6.6-3 1.5-4-.2-.4-.7-1.9.1-4 0 0 1.3-.4 4.1 1.5 1.2-.3 2.5-.5 3.8-.5 1.3 0 2.6.2 3.8.5 2.9-1.9 4.1-1.5 4.1-1.5.8 2.1.3 3.6.1 4 1 1 1.5 2.4 1.5 4 0 5.8-3.5 7-6.8 7.4.5.5 1 1.4 1 2.8v4.1c0 .4.3.9 1 .7 6-2 10.2-7.6 10.2-14.2C40 16.7 33.3 10 25 10z"/></svg>
         <span><strong>Backend</strong></span>
@@ -28,8 +28,8 @@
         </svg>
         <span class="ml-2"><strong>Carpeta de Proyecto</strong></span>
       </a>
-    </p>
-    <p><i>Universidad Maimónides. 2022.</i></p>
+    </div>
+    <p class="mt-2"><i>Universidad Maimónides. 2022.</i></p>
   </div>
 </footer>
 </template>
@@ -69,6 +69,22 @@ h3 {
   width: 100vw;
   background-color: $dark;
   color: $white;
+}
+@media(max-width: 768px) {
+  .footer-title {
+    gap: 0.4em;
+    flex-direction: column;
+  }
+  .container {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2em;
+    gap: 0.8em;
+    & a {
+      margin: 0 !important;
+    }
+  }
 }
 a{
   color: $primary;
